c938e9d50828ba8800cd50f1db73316d5490925e
Znyd <mailboxofnabil@gmail.com>
2025-06-19 02:40:34 +0600

    rest of the test

--- task_fine_tuning/vid_analysis.py
+++ task_fine_tuning/vid_analysis.py
@@ -16,24 +16,25 @@ def parse_time(timestr):
 with open("irrelevant.json", "r", encoding='utf-8') as f:
     loaded_file = json.load(f)
 
-
+total_ln = 0
 dataset_files = []
 for i in range(len(loaded_file)):
     for seg in loaded_file[i]['irrelevant_segments']:
         start_time =  parse_time(seg['start_time'][0:8])
         end_time =  parse_time(seg['end_time'][0:8])
         vid_ln = end_time - start_time
-        m, s = divmod(vid_ln, 60)
-        if m > 3:
+        total_ln+=vid_ln
+        # m, s = divmod(vid_ln, 60)
+        # if m > 3:
         #  print(f"✅Starting Video: {i}")
         #  print(f"yt_link: {loaded_file[i]['link']}")
-         m1, s1 = divmod(start_time, 60)
-         m2, s2 = divmod(end_time, 60)
+        #  m1, s1 = divmod(start_time, 60)
+        #  m2, s2 = divmod(end_time, 60)
     
-         start_time = f"{(m1)}M:{(s1)}S"
-         end_time = f"{(m2)}M:{(s2)}S"
-         print(f"Start Time: {start_time} -- End Time: {end_time}")
-         print(f"Vid length {m}m:{s}s")
+        #  start_time = f"{(m1)}M:{(s1)}S"
+        #  end_time = f"{(m2)}M:{(s2)}S"
+        #  print(f"Start Time: {start_time} -- End Time: {end_time}")
+        #  print(f"Vid length {m}m:{s}s")
         #  print(f"✅Ending Video: {i}")
 
 #     yt_id = loaded_file[i]['link'][32:43]
@@ -44,5 +45,5 @@ for i in range(len(loaded_file)):
 # for file in files:
 #     if str(file)[11:] not in dataset_files:
 #         file.unlink()
-
+print(total_ln)
 print(len(loaded_file))
