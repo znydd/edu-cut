61a10fa5bfc93d4696a4bcd285cf72216bcb9681
Znyd <mailboxofnabil@gmail.com>
2025-07-31 08:09:04 +0600

    video and audio download different func

--- edu_cut/preproc/pre_processor.py
+++ edu_cut/preproc/pre_processor.py
@@ -12,26 +12,13 @@ class PreProcessor:
             self.yt_id
         )
 
-    def download(self) -> None:
+    def download_video(self) -> None:
         video_opts = {
             "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
             "outtmpl": str(Path(self.video_dir) / f"{self.yt_id}.%(ext)s"),
             "quiet": False,
         }
-        audio_opts = {
-            "format": "bestaudio/best",
-            "outtmpl": str(Path(self.audio_dir) / f"{self.yt_id}.%(ext)s"),
-            "postprocessors": [
-                {
-                    "key": "FFmpegExtractAudio",
-                    "preferredcodec": "mp3",
-                    "preferredquality": "192",
-                }
-            ],
-            "quiet": False,
-        }
         video_file = self.video_dir.joinpath(f"{self.yt_id}.mp4")
-        audio_file = self.audio_dir.joinpath(f"{self.yt_id}.mp3")
 
         if self.storage.exist(video_file):
             print(f"\n---- {self.yt_id}.mp4 Already Exist")
@@ -46,6 +33,20 @@ class PreProcessor:
             except Exception as e:
                 print(f"An unexpected error occurred during video download: {e}")
 
+    def download_audio(self) -> None:
+        audio_opts = {
+            "format": "bestaudio/best",
+            "outtmpl": str(Path(self.audio_dir) / f"{self.yt_id}.%(ext)s"),
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                }
+            ],
+            "quiet": False,
+        }
+        audio_file = self.audio_dir.joinpath(f"{self.yt_id}.mp3")
         if self.storage.exist(audio_file):
             print(f"\n---- {self.yt_id}.mp3 Already Exist")
         else:
@@ -60,7 +61,10 @@ class PreProcessor:
                 print(f"An unexpected error occurred during audio download: {e}")
 
 
+
+
 x = PreProcessor(
     "https://www.youtube.com/watch?v=cmjmsgkbZuw&list=PL9aZtK5kh5WcyVuwOF80eE88U5K7Ctva-&index=9"
 )
-x.download()
+x.download_video()
+x.download_audio()
